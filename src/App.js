a0470ebfa73e9787e012bd8087d92086f489b4b7
kk77777 <kaushikgattani@gmail.com>
2020-05-01 17:10:37 +0530

    making api request

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Cards from './components/Cards/Cards';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import Chart from './components/Chart/Chart';
 import styles from './App.module.css';
+import { fetchData } from './api/index';
 
 export class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
