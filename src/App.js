92304206349d3b4c8ee9e5c9d0f923a285a67fc8
kk77777 <kaushikgattani@gmail.com>
2020-05-01 19:30:24 +0530

    passing data to cards

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import styles from './App.module.css';
 import { fetchData } from './api/index';
 
 export class App extends Component {
+  state = {
+    data: {},
+  };
+
   async componentDidMount() {
     const data = await fetchData();
-    console.log(data);
+    this.setState({ data });
   }
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
