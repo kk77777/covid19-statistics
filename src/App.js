16d208bda3ec0e8bca930c2060f2799dc7399a05
kk77777 <kaushikgattani@gmail.com>
2020-05-01 15:54:07 +0530

    importing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import Cards from './components/Cards/Cards';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import Chart from './components/Chart/Chart';
+
 export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
